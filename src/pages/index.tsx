a6e949656f644920980ba829cbf9c4a7619f5607
alfianlensun <59629347+alfianlensun@users.noreply.github.com>
2022-01-06 08:19:40 +0800

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function HomePage(){
+	return (
+        <div className="w-screen h-screen bg-black relative">
+            AWKJDHAWD
+        </div>
+	)
+}
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            fontSize: {
+                xss: '.6rem'
+            },
+            colors: {
+                'custom-container-gray': '#e9edf2',
+            },
+            width: {
+                'fit-content': 'fit-content'
+            },
+            height: {
+                'fit-content': 'fit-content'
+            }
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
